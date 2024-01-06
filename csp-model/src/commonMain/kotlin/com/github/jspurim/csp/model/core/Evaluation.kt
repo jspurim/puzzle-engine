51a61dc3163847d02cc591fe81b1beb36c33a6ce
Jerónimo Spuri Murguía <jspurim@gmail.com>
2024-01-06 14:41:55 +0000

    Refactor [Evaluation] to use operator overloading for set and get.

--- csp-model/src/commonMain/kotlin/com/github/jspurim/csp/model/core/Evaluation.kt
+++ csp-model/src/commonMain/kotlin/com/github/jspurim/csp/model/core/Evaluation.kt
@@ -12,7 +12,7 @@ class Evaluation {
     /**
      * Set the value of the provided variable.
      */
-    fun <T> setValue(variable : Variable<T>, value : T) {
+    operator fun <T> set(variable : Variable<T>, value : T) : Unit {
         if(!variable.domain.isValidValue(value)){
             throw IllegalArgumentException("The value $value is not in the domain of variable $variable, which is ${variable.domain}.")
         }
@@ -23,7 +23,7 @@ class Evaluation {
      * Retrieve the value of the provided variable.
      */
     @Suppress("UNCHECKED_CAST")
-    fun <T> getValue(variable: Variable<T>) : T? {
+    operator fun <T> get(variable: Variable<T>) : T? {
         return data[variable] as T?
     }
 }
--- csp-model/src/commonTest/kotlin/com/github/jspurim/csp/model/core/EvaluationTests.kt
+++ csp-model/src/commonTest/kotlin/com/github/jspurim/csp/model/core/EvaluationTests.kt
@@ -2,7 +2,6 @@ package com.github.jspurim.csp.model.core
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
-import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 
@@ -17,52 +16,52 @@ class EvaluationTests : FreeSpec({
 
     "Empty evaluation should return null when fetching variable" {
         val emptyEvaluation = Evaluation()
-        emptyEvaluation.getValue(intVariable_1) shouldBe null
+        emptyEvaluation[intVariable_1] shouldBe null
     }
 
     "Evaluation should allow setting and retrieving the values" - {
         "for a  single variable" {
             val evaluation = Evaluation()
-            evaluation.setValue(intVariable_1, 42)
-            evaluation.getValue(intVariable_1) shouldBe 42
+            evaluation[intVariable_1] = 42
+            evaluation[intVariable_1] shouldBe 42
         }
 
         "for a  single variable, retrieving via a different but equal variable" {
             val evaluation = Evaluation()
-            evaluation.setValue(intVariable_1, 42)
-            evaluation.getValue(duplicateIntVariable_1) shouldBe 42
+            evaluation[intVariable_1] = 42
+            evaluation[duplicateIntVariable_1] shouldBe 42
         }
 
 
         "for a  multiple variables of the same type" {
             val evaluation = Evaluation()
-            evaluation.setValue(intVariable_1, 42)
-            evaluation.setValue(intVariable_2, 43)
-            evaluation.getValue(intVariable_1) shouldBe 42
-            evaluation.getValue(intVariable_2) shouldBe 43
+            evaluation[intVariable_1] = 42
+            evaluation[intVariable_2] = 43
+            evaluation[intVariable_1] shouldBe 42
+            evaluation[intVariable_2] shouldBe 43
         }
 
         "for a  multiple variables of different types, with same ids" {
             val evaluation = Evaluation()
-            evaluation.setValue(intVariable_1, 42)
-            evaluation.setValue(charVariable_1, 'A')
-            evaluation.getValue(intVariable_1) shouldBe 42
-            evaluation.getValue(charVariable_1) shouldBe 'A'
+            evaluation[intVariable_1] = 42
+            evaluation[charVariable_1] = 'A'
+            evaluation[intVariable_1] shouldBe 42
+            evaluation[charVariable_1] shouldBe 'A'
         }
     }
 
     "Evaluation should allow overriding the value of a variable" -{
         "through the same variable instance" {
             val evaluation = Evaluation()
-            evaluation.setValue(intVariable_1, 41)
-            evaluation.setValue(intVariable_1, 42)
-            evaluation.getValue(intVariable_1) shouldBe 42
+            evaluation[intVariable_1] = 41
+            evaluation[intVariable_1] = 42
+            evaluation[intVariable_1] shouldBe 42
         }
         "through an equivalent variable instance" {
             val evaluation = Evaluation()
-            evaluation.setValue(intVariable_1, 41)
-            evaluation.setValue(duplicateIntVariable_1, 42)
-            evaluation.getValue(intVariable_1) shouldBe 42
+            evaluation[intVariable_1] = 41
+            evaluation[duplicateIntVariable_1] = 42
+            evaluation[intVariable_1] shouldBe 42
         }
     }
 
@@ -70,9 +69,10 @@ class EvaluationTests : FreeSpec({
         "for as sudoku digit variable" - {
             listOf(0,10,-1).forEach {
                 "should throw when setting the value to $it" {
-                    val evaluation = Evaluation()
                     shouldThrow<IllegalArgumentException> {
-                        evaluation.setValue(sudokuVariable_1, it)
+                        val evaluation = Evaluation()
+                        evaluation[sudokuVariable_1] = it
+                        return@shouldThrow Unit
                     }
                 }
             }
