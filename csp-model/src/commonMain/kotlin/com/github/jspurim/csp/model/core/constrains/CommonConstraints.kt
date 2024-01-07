7d722ae2472858f18f8e038ba34294d290463146
Jerónimo Spuri Murguía <jspurim@gmail.com>
2024-01-07 17:11:31 +0000

    Add [ValueInConstraint] to restrict variable values to an allowlist.

--- csp-model/src/commonMain/kotlin/com/github/jspurim/csp/model/core/constrains/CommonConstraints.kt
+++ csp-model/src/commonMain/kotlin/com/github/jspurim/csp/model/core/constrains/CommonConstraints.kt
@@ -3,7 +3,12 @@ package com.github.jspurim.csp.model.core.constrains
 import com.github.jspurim.csp.model.core.Constraint
 import com.github.jspurim.csp.model.core.Evaluation
 import com.github.jspurim.csp.model.core.Variable
+import kotlinx.collections.immutable.ImmutableSet
+import kotlinx.collections.immutable.persistentHashSetOf
 
+/**
+ * A constraint prohibiting two variables from taking the same value.
+ */
 class NotEqualConstraint<T>(private val var1: Variable<T>, private val var2: Variable<T>) : Constraint {
     override fun isSatisfied(evaluation: Evaluation): Boolean {
         val val1 = evaluation[var1]
@@ -12,3 +17,18 @@ class NotEqualConstraint<T>(private val var1: Variable<T>, private val var2: Var
         return val1 != val2
     }
 }
+
+/**
+ * A constraint forcing the value of a variable to be in a specific set of allowed values.
+ */
+class ValueInConstraint<T>(private val variable: Variable<T>, private val allowedValues : ImmutableSet<T>) : Constraint {
+    override fun isSatisfied(evaluation: Evaluation): Boolean {
+        val value = evaluation[variable] ?: return true
+        return value in allowedValues
+    }
+}
+
+/**
+ * Creates a [Constraint] forcing a given variable to take a specific value.
+ */
+fun <T> givenValueConstraint(variable: Variable<T>, value: T) = ValueInConstraint(variable, persistentHashSetOf(value))
--- csp-model/src/commonTest/kotlin/com/github/jspurim/csp/model/core/constraints/CommonConstraintsTests.kt
+++ csp-model/src/commonTest/kotlin/com/github/jspurim/csp/model/core/constraints/CommonConstraintsTests.kt
@@ -1,10 +1,15 @@
 package com.github.jspurim.csp.model.core.constraints
 
+import com.github.jspurim.csp.model.core.Domains
 import com.github.jspurim.csp.model.core.Evaluation
+import com.github.jspurim.csp.model.core.SimpleFiniteVariable
 import com.github.jspurim.csp.model.core.constrains.NotEqualConstraint
+import com.github.jspurim.csp.model.core.constrains.ValueInConstraint
+import com.github.jspurim.csp.model.core.constrains.givenValueConstraint
 import com.github.jspurim.csp.model.core.simpleIntVariable
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import kotlinx.collections.immutable.persistentHashSetOf
 
 class NotEqualConstraintTests : FreeSpec({
 
@@ -57,6 +62,45 @@ class NotEqualConstraintTests : FreeSpec({
             evaluation[varB] = 42
             evaluation[varC] = 42
             abNotEqualConstrant.isSatisfied(evaluation) shouldBe true
+        }
+    }
+})
+
+class ValueInConstraintTests : FreeSpec({
+    val variable = SimpleFiniteVariable(1, Domains.SUDOKU_DIGITS)
+    "Constraint retraining value to be either 2, 3, 5, or 7" - {
+        val valueMustBePrime = ValueInConstraint(variable, persistentHashSetOf(2,3,5,7))
+        listOf(2,3,5,7).forEach {
+            "Should accept $it as value" {
+                val evaluation = Evaluation()
+                evaluation[variable] = it
+                valueMustBePrime.isSatisfied(evaluation) shouldBe true
+            }
+        }
+
+        listOf(1,4,6,8,9).forEach {
+            "Should reject $it as value" {
+                val evaluation = Evaluation()
+                evaluation[variable] = it
+                valueMustBePrime.isSatisfied(evaluation) shouldBe false
+            }
+        }
+    }
+
+    "Constraint forcing variable to be specifically 5" - {
+        val valueMustBeFive = givenValueConstraint(variable, 5)
+        "Should accept 5 as value" {
+            val evaluation = Evaluation()
+            evaluation[variable] = 5
+            valueMustBeFive.isSatisfied(evaluation) shouldBe true
+        }
+
+        listOf(1,2,3,4,6,7,8,9).forEach {
+            "Should reject $it as value" {
+                val evaluation = Evaluation()
+                evaluation[variable] = it
+                valueMustBeFive.isSatisfied(evaluation) shouldBe false
+            }
         }
     }
 })
